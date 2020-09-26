b79a2f6cd7028bf38f2219df595fb9a56ca664b3
djerik <djerik@gmail.com>
2020-09-26 23:10:07 +0200

    Update node_helper.js
    Added support for yesterday

--- node_helper.js
+++ node_helper.js
@@ -14,11 +14,24 @@ module.exports = NodeHelper.create({
 		console.log("Notification: " + notification + " Payload: " + payload);
 
 		if(notification === "GET_SOLAR") {
+			let yesterday = (function(d){ d.setDate(d.getDate()-1); return d})(new Date);
+			const yesterday_date = yesterday.toISOString().substring(0, 10);
+			
 			var solarEdgeUrl = payload.config.url + payload.config.siteId + "/overview?api_key=" + payload.config.apiKey;
+			const solarEdgeYesterdayUrl = payload.config.url + payload.config.siteId + "/energy?timeUnit=DAY&endDate=" + yesterday_date + "&startDate=" + yesterday_date + "&api_key=" + payload.config.apiKey;
 			request(solarEdgeUrl, function (error, response, body) {
 				if (!error && response.statusCode == 200) {
 					var jsonData = JSON.parse(body);
-				        self.sendSocketNotification("SOLAR_DATA", jsonData);
+					request(solarEdgeYesterdayUrl, function (error, response, body)
+					{
+						if (!error && response.statusCode == 200)
+						{
+							let yesterData = JSON.parse(body);
+							jsonData.yesterday = yesterData.energy.values[0].value;
+						}
+
+						self.sendSocketNotification("SOLAR_DATA", jsonData);
+					});
 				}
 			});
 		}
